a64915cfd455bf15a6e3d0a8badbdd7ed58cdf41
sunilmeena26 <sunilmeena641085@gmail.com>
2025-03-21 15:03:16 +0530

    [Sunil Meena] Add resources folder to added application.properties, schema.sql, and data.sql for Spring JDBC demo with alien table creation and sample data insertion

---
+++ SpringJDBCDemo/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into alien (id,name,tech) values (101,'Karan','AI');
+insert into alien (id,name,tech) values (102,'Ravi','BlockChain');
+insert into alien (id,name,tech) values (103,'Mohit','IoT');
