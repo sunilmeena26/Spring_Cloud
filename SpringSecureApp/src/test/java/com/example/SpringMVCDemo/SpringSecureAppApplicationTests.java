72668dc0c4d16570b89ccad85fbc837b5850a416
sunilmeena26 <sunilmeena641085@gmail.com>
2025-03-22 11:05:27 +0530

    [Sunil Meena] Add SpringSecureApp project to use spring security concept

--- SpringSecureApp/src/test/java/com/example/SpringMVCDemo/SpringSecureAppApplicationTests.java
+++ SpringSecureApp/src/test/java/com/example/SpringMVCDemo/SpringSecureAppApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.SpringMVCDemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//This test class for testing SpringSecureProject project
 @SpringBootTest
 class SpringSecureAppApplicationTests {
 
